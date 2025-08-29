af393840aac7139b1b1f4fe0e2b62ba0285f4328
WolfSyn <115028079+WolfSyn@users.noreply.github.com>
2025-08-29 17:24:16 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
